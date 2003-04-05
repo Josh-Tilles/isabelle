42fcdd57ff2e8953c929e6cdb798c655391b7a89
kleing <devnull@localhost>
2003-04-05 17:25:06 +0200

    cleanup, mark old (<1994) deleted files as dead

--- src/HOLCF/HOLCF.thy
+++
@@ -1,9 +0,0 @@
-(*  Title:      HOLCF/HOLCF.thy
-    ID:         $Id$
-    Author:     Franz Regensburger
-    License:    GPL (GNU GENERAL PUBLIC LICENSE)
-
-Top theory for HOLCF system.
-*)
-
-HOLCF = Sprod3 + Ssum3 + Up3 + Lift + Discrete + One + Tr
