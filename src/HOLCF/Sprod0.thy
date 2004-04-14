3a76493d07cdf498b04aad8429dccdc36a2c8344
kleing <devnull@localhost>
2004-04-14 14:13:05 +0200

    use more symbols in HTML output

---
+++ src/HOLCF/Sprod0.thy
@@ -0,0 +1,39 @@
+(*  Title:      HOLCF/Sprod0.thy
+    ID:         $Id$
+    Author:     Franz Regensburger
+    License:    GPL (GNU GENERAL PUBLIC LICENSE)
+
+Strict product with typedef.
+*)
+
+Sprod0 = Cfun3 +
+
+constdefs
+  Spair_Rep     :: ['a,'b] => ['a,'b] => bool
+ "Spair_Rep == (%a b. %x y.(~a=UU & ~b=UU --> x=a  & y=b ))"
+
+typedef (Sprod)  ('a, 'b) "**" (infixr 20) = "{f. ? a b. f = Spair_Rep (a::'a) (b::'b)}"
+
+syntax (xsymbols)
+  "**"		:: [type, type] => type	 ("(_ \\<otimes>/ _)" [21,20] 20)
+syntax (HTML output)
+  "**"		:: [type, type] => type	 ("(_ \\<otimes>/ _)" [21,20] 20)
+
+consts
+  Ispair        :: "['a,'b] => ('a ** 'b)"
+  Isfst         :: "('a ** 'b) => 'a"
+  Issnd         :: "('a ** 'b) => 'b"  
+
+defs
+   (*defining the abstract constants*)
+
+  Ispair_def    "Ispair a b == Abs_Sprod(Spair_Rep a b)"
+
+  Isfst_def     "Isfst(p) == @z.        (p=Ispair UU UU --> z=UU)
+                &(! a b. ~a=UU & ~b=UU & p=Ispair a b   --> z=a)"  
+
+  Issnd_def     "Issnd(p) == @z.        (p=Ispair UU UU  --> z=UU)
+                &(! a b. ~a=UU & ~b=UU & p=Ispair a b    --> z=b)"  
+
+
+end
---
+++ src/HOLCF/Ssum0.thy
@@ -0,0 +1,39 @@
+(*  Title:      HOLCF/Ssum0.thy
+    ID:         $Id$
+    Author:     Franz Regensburger
+    License:    GPL (GNU GENERAL PUBLIC LICENSE)
+
+Strict sum with typedef
+*)
+
+Ssum0 = Cfun3 +
+
+constdefs
+  Sinl_Rep      :: ['a,'a,'b,bool]=>bool
+ "Sinl_Rep == (%a.%x y p. (a~=UU --> x=a & p))"
+  Sinr_Rep      :: ['b,'a,'b,bool]=>bool
+ "Sinr_Rep == (%b.%x y p.(b~=UU --> y=b & ~p))"
+
+typedef (Ssum)  ('a, 'b) "++" (infixr 10) = 
+	"{f.(? a. f=Sinl_Rep(a::'a))|(? b. f=Sinr_Rep(b::'b))}"
+
+syntax (xsymbols)
+  "++"		:: [type, type] => type	("(_ \\<oplus>/ _)" [21, 20] 20)
+syntax (HTML output)
+  "++"		:: [type, type] => type	("(_ \\<oplus>/ _)" [21, 20] 20)
+
+consts
+  Isinl         :: "'a => ('a ++ 'b)"
+  Isinr         :: "'b => ('a ++ 'b)"
+  Iwhen         :: "('a->'c)=>('b->'c)=>('a ++ 'b)=> 'c"
+
+defs   (*defining the abstract constants*)
+  Isinl_def     "Isinl(a) == Abs_Ssum(Sinl_Rep(a))"
+  Isinr_def     "Isinr(b) == Abs_Ssum(Sinr_Rep(b))"
+
+  Iwhen_def     "Iwhen(f)(g)(s) == @z.
+                                    (s=Isinl(UU) --> z=UU)
+                        &(!a. a~=UU & s=Isinl(a) --> z=f$a)  
+                        &(!b. b~=UU & s=Isinr(b) --> z=g$b)"  
+
+end
