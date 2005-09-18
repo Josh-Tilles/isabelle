3645a3da42435df9048cf30a231079c859ed1d31
wenzelm <devnull@localhost>
2005-09-18 14:25:48 +0200

    converted to Isar theory format;

---
+++ src/FOLP/ex/If.thy
@@ -0,0 +1,13 @@
+(* $Id$ *)
+
+theory If
+imports FOLP
+begin
+
+constdefs
+  if :: "[o,o,o]=>o"
+  "if(P,Q,R) == P&Q | ~P&R"
+
+ML {* use_legacy_bindings (the_context ()) *}
+
+end
---
+++ src/FOLP/ex/Nat.thy
@@ -0,0 +1,42 @@
+(*  Title:      FOLP/ex/nat.thy
+    ID:         $Id$
+    Author:     Lawrence C Paulson, Cambridge University Computer Laboratory
+    Copyright   1992  University of Cambridge
+*)
+
+header {* Theory of the natural numbers: Peano's axioms, primitive recursion *}
+
+theory Nat
+imports FOLP
+begin
+
+typedecl nat
+arities nat         :: "term"
+consts  "0"         :: "nat"    ("0")
+        Suc         :: "nat=>nat"
+        rec         :: "[nat, 'a, [nat,'a]=>'a] => 'a"
+        "+"         :: "[nat, nat] => nat"              (infixl 60)
+
+  (*Proof terms*)
+       nrec         :: "[nat,p,[nat,p]=>p]=>p"
+       ninj         :: "p=>p"
+       nneq         :: "p=>p"
+       rec0         :: "p"
+       recSuc       :: "p"
+
+axioms
+  induct:     "[| b:P(0); !!x u. u:P(x) ==> c(x,u):P(Suc(x))
+              |] ==> nrec(n,b,c):P(n)"
+
+  Suc_inject: "p:Suc(m)=Suc(n) ==> ninj(p) : m=n"
+  Suc_neq_0:  "p:Suc(m)=0      ==> nneq(p) : R"
+  rec_0:      "rec0 : rec(0,a,f) = a"
+  rec_Suc:    "recSuc : rec(Suc(m), a, f) = f(m, rec(m,a,f))"
+  add_def:    "m+n == rec(m, n, %x y. Suc(y))"
+
+  nrecB0:     "b: A ==> nrec(0,b,c) = b : A"
+  nrecBSuc:   "c(n,nrec(n,b,c)) : A ==> nrec(Suc(n),b,c) = c(n,nrec(n,b,c)) : A"
+
+ML {* use_legacy_bindings (the_context ()) *}
+
+end
