8dbe6d7bade9f2382449637dda0b230a26b75397
Josh Tilles <MerelyAPseudonym@gmail.com>
2014-02-15 22:21:39 -0500

    Mysterious failure (once fixed, amend previous commit)

--- src/HOL/SAT.thy
+++ src/HOL/SAT.thy
@@ -8,7 +8,7 @@ Basic setup for the 'sat' and 'satx' tactics.
 header {* Reconstructing external resolution proofs for propositional logic *}
 
 theory SAT
-imports IHOL
+imports CHOL
 begin
 
 ML_file "Tools/prop_logic.ML"
